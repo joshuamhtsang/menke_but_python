caf18d4c86fa4480080663d0d50b76ca6f92b994
Joshua Tsang <joshua.tsang@soundmouse.com>
2023-12-29 18:29:09 +0000

    added a normalised, 2d normal distruibution

--- chapter_3/normal_distribution.py
+++ chapter_3/normal_distribution.py
@@ -5,7 +5,8 @@ import numpy as np
 def normal_distribution(x, mean, sigma):
     return 1.0/((np.sqrt(2.0*np.pi)*sigma))*np.exp(-np.power(x-mean,2)/(2*sigma*sigma))
 
-# Eqn (3.24) in Menke book
+# Computer normalized normal distribution in 2 dimensions
+# i.e. eqn (3.24) in Menke book
 def normal_distribution_2d(x_1, x_2, xbar_1, xbar_2, sigma_1, sigma_2):
     norm = 1.0/(2*np.pi*sigma_1*sigma_2)
     p_1 = np.exp(-np.power(x_1-xbar_1,2)/(2*sigma_1*sigma_1))
